99d312216c4db730729ee41ef7218f5eaae13dfb
Steven Watkins <gromit840@gmail.com>
2013-12-17 23:53:49 -0500

    finished 10, added clear method to screen class

--- src/com/thecherno/rain/graphics/Screen.java
+++ src/com/thecherno/rain/graphics/Screen.java
@@ -11,4 +11,20 @@ public class Screen {
 		this.height = height;
 		pixels = new int[width * height];
 	}
+	
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++){
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render() {//add arguments later
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++){
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
+	}
+	
+	
 }
